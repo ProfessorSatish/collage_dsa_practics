218c154582748642ccf72ec4b28e01c823eb767f
rohan <satish.yadav.amila@gmail.com>
2025-10-28 14:31:55 +0530

    first

---
+++ listdatatype.py
@@ -0,0 +1,27 @@
+'''A list in python is a collection of data type that is ordered, changeable (mutable ), and allows duplicate element . It 
+use to stored multiple iteam in single variable'''
+
+#syntex
+#my_list=[iteam1,iteam2,iteam3]
+#ex
+fruite=["apple ","banana","cherry"]
+print(fruite)
+#output [apple, banana, cherry]
+
+''' some opreation in the list '''
+fruite.append("mango")
+print(fruite)
+fruite.append("amrud")
+print(fruite)
+fruite.append("orange")
+print(fruite)
+# next opration
+#insert opration insert the iteam in the specifed postion 
+''' now we are creat a new list'''
+a=['ram','shyam','durgesh','kailash']
+#I want to insert into the shyam postion of satish name 
+a.insert(1,"satish")
+print(a)
+print(a)
+a.insert(2,"shyam")
+print(a)
