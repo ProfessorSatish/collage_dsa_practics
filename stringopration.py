a28e463aa96041858871f2893f996f0f66c7519d
rohan <satish.yadav.amila@gmail.com>
2025-10-31 00:26:32 +0530

    until the replace

--- stringopration.py
+++ stringopration.py
@@ -18,3 +18,27 @@ s="satish "
 print(s*3)
 b="ha"
 print(b*3)
+
+#indexing in the python string opration
+s="hello"
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+
+#sclicing the string 
+v="hello my name is satish yadav"
+#sclicing the words
+print(v[0::3]  )
+
+#find the sub string 
+s="satish yadav "
+
+print(s.find("ya"))
+
+#replace the words
+s="java is my favroatie subject"
+print(s.replace("java","python"))
+print(s.replace("java","php"))
+print(s)
