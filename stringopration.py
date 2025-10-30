514683720b4a254d543a36e1c877c1e8ed5fc957
rohan <satish.yadav.amila@gmail.com>
2025-10-31 00:12:05 +0530

    util the repation

--- stringopration.py
+++ stringopration.py
@@ -5,3 +5,16 @@ print(len(s))
 print(len(name))
 
 #concatenation of the string in the python
+'''join the two string in the python'''
+first ="satish "
+middle=" "
+lastname="yadav"
+concatenation=first+middle+lastname
+print(concatenation)
+
+#repation of the string 
+''' repeats a string multiple times'''
+s="satish "
+print(s*3)
+b="ha"
+print(b*3)
