20e40495e7f3b747640656e7dd8e46fe6f55bb66
rohan <satish.yadav.amila@gmail.com>
2025-11-01 23:09:52 +0530

    while loops commit

--- loops.py
+++ loops.py
@@ -5,3 +5,12 @@ for i in range(1,11):
 t=int(input("Enter the number that you want to table "))
 for i in range(1,11):
     print(f"{t} x {i} = {t*i}",)
+
+
+#while loops
+# print the table fro the while loops of help
+s=int(input("Enter the number that you want to table "))
+i= 1
+while i<=10:
+    print(f"{s} x {i} ={s*i}")
+    i+=1
