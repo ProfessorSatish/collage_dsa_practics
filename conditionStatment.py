23ca64dae8ce89a1451aa7bf55cc6afc20ce3244
rohan <satish.yadav.amila@gmail.com>
2025-11-03 00:37:07 +0530

    that is my first commit

---
+++ conditionStatment.py
@@ -0,0 +1,31 @@
+#what is condtion statement in the python
+'''if,elif,else ,break,continue, pass'''
+#these are the keyword that are used in the python programming
+
+for i in range(1,6):
+    if i%2==0:
+        print(f"i is even {i}")
+    
+
+# now elif keyword used
+for i in range(1,10):
+    if i==1:
+        print(f"i is One")
+    elif i==2:
+        print(f"i is Two")
+    elif i==3:
+        print(f"i is Three")
+    elif i==4:
+        print(f"i is Four")
+    else:
+        print("other number ")
+
+# else statement
+for i in range(1,4):
+    if i ==2:
+        print("first")
+    else:
+        print("other number")
+
+# break statement in the python
+
