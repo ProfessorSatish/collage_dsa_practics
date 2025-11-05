f4482de82c0250670f240d8f7837552775576c5f
rohan <satish.yadav.amila@gmail.com>
2025-11-05 11:48:48 +0530

    that is last commit until the pass, break

--- loops.py
+++ loops.py
@@ -14,3 +14,31 @@ i= 1
 while i<=10:
     print(f"{s} x {i} ={s*i}")
     i+=1
+
+#break statement
+for i in range(1,6):
+    if (i==2):
+        break
+    print(i)
+
+#Continue statement
+for i in range(1,6):
+    if(i==5):
+        continue
+    print(i)
+
+for i in range(1,6):
+    if (i==2):
+        pass
+    print(i)
+
+
+#if elif else and the break and continue statement
+for i in range(1,6):
+    if i==2:
+        print("skipping ",i)
+        continue
+    elif(i==5):
+        break
+    else:
+        print("Number is ",i)
