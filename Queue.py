8e3bb78419b2c28f9abcd3f5a15fa1bfcb970918
rohan <satish.yadav.amila@gmail.com>
2025-11-06 00:38:32 +0530

    logical implementation of the queue

--- Queue.py
+++ Queue.py
@@ -52,3 +52,54 @@
 # d.dequeue()
 # d.dequeue()
 # d.is_empty()
+
+''' logical implementation of the queue'''
+class Queue:
+    def __init__(self,size):
+        self.size=size
+        self.queue=[0]*size
+        self.front=-1
+        self.rear=-1
+    def enqueue(self,data):
+        if self.rear==self.size-1:
+            print("Queue is overflow")
+            return
+        if self.front==-1:
+            self.front=0
+            
+            
+        self.rear+=1
+        self.queue[self.rear]=data
+        print(f"Enqued {data}")
+        print(self.queue)
+
+    def dequeue(self):
+        if self.front==-1:
+            print("queue is underflow")
+            return
+        value=self.queue[self.front]
+        self.front +=1
+        print(f"Delete the {value}")
+    def peek(self):
+        if self.queue==-1:
+            print("queue is under flow")
+            return
+        print(f"The element is {self.front}")
+    def display(self):
+        if self.front==-1 or self.front>self.rear:
+            print("queue is empty")
+            return
+        print("Queue (front ->rear)" ,end=" ")
+        for i in range(self.front,self.rear+1):
+            print(self.queue[i],end =" ")
+        print()
+    
+
+b=Queue(5)
+b.enqueue(33)
+b.enqueue(23)
+b.enqueue(99)
+b.enqueue(100) 
+b.dequeue()     
+b.display()
+b.dequeue()
